5115ae13d56b22349089be8a993152b0595565ff
SubhodeepRoy17 <subhodeeproy37@gmail.com>
2025-08-27 20:25:16 +0530

    minor change

--- Middleware/UserAuthorizationMiddleware.js
+++ Middleware/UserAuthorizationMiddleware.js
@@ -2,6 +2,8 @@ const userAuthorizationHandler = (...allowedRoles) => {
     return (req, res, next) => {
         const userRole = req?.user?.role;
 
+        console.log('User role:', req.user.role);
+
         if (!allowedRoles.includes(userRole)) {
             return res.status(403).json({
                 status: false,
